76e5c5f0a9c32f5b8d4fcc0dc6d2a9a2c2873909
steve-chen-nyc <steve@Steves-MacBook-Pro.local>
2016-03-14 01:40:36 -0400

    new static

--- bcrecruit/wsgi.py
+++ bcrecruit/wsgi.py
@@ -12,11 +12,11 @@ import sys
 
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
-from django.contrib.staticfiles.handlers import StaticFilesHandler
 
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bcrecruit.settings")
 
-application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
