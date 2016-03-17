5eab380b71b1f28e7ea4e211f7ee6e41df0c84f3
steve-chen-nyc <steve@Steves-MacBook-Pro.local>
2016-03-16 20:38:10 -0400

    tweet count now correct on page load. Turns red when char limit hits

--- bcrecruit/settings.py
+++ bcrecruit/settings.py
@@ -21,6 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
+
 SECRET_KEY = environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -144,6 +145,7 @@ SOCIAL_AUTH_TWITTER_SECRET = environ.get('CONSUMER_SECRET')         # Twitter AP
 
 TWITTER_ACCESS_TOKEN = environ.get('ACCESS_TOKEN')                  # Twitter API Access Token
 TWITTER_ACCESS_TOKEN_SECRET = environ.get('ACCESS_TOKEN_SECRET')    # Twitter API Access Secret
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
