0c871c02d5ed6820c32d4ec34bcc8250b79353f7
steve-chen-nyc <steve.l.chen@gmail.com>
2016-03-17 22:39:50 -0400

    added viewport for mobile and links in twitter feed can now be clicked

--- bcrecruit/settings.py
+++ bcrecruit/settings.py
@@ -24,7 +24,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = environ.get('DJANGO_SECRET_KEY')
 
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
